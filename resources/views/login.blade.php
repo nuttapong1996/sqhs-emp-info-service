4f990f4a9b0bd7b17fc19cb7a1f9f4c1bdae1399
N0m@d-Xii <hugewolf1996@gmail.com>
2026-04-23 21:06:23 +0700

    แก้ไข livewire script

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @googlefonts
+    @livewireScripts
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>{{ config('app.name') }}</title>
 </head>
